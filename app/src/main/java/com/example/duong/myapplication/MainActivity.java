a521f428ea206393429b4b40779ac421f28e526b
duong <duongnvt2110@gmail.com>
2018-12-13 00:00:31 +0700

    Add get GeoLocation GPS

--- app/src/main/java/com/example/duong/myapplication/MainActivity.java
+++ app/src/main/java/com/example/duong/myapplication/MainActivity.java
@@ -13,16 +13,39 @@ import android.widget.TextView;
 import android.widget.Button;
 import java.util.ArrayList;
 import java.util.List;
-
+import 	android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.Toast;
+import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.Manifest;
+import android.provider.Settings;
+import android.os.Build;
 import com.example.duong.myapplication.LocationList;
 import com.example.duong.myapplication.ReviewList;
+
+
+
 public class MainActivity extends AppCompatActivity  {
 
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected int MY_PERMISSION_ACCESS_COARSE_LOCATION = 1;
+    protected Context context;
+    TextView txtLat;
+    String lat;
+    String provider;
+    protected String latitude,longitude;
+    protected boolean gps_enabled,network_enabled;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Call View  Location
         final Button btnView = (Button) findViewById(R.id.btn_view);
         btnView.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -30,6 +53,49 @@ public class MainActivity extends AppCompatActivity  {
             }
         });
 
+//        Add if to check permission.
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    MY_PERMISSION_ACCESS_COARSE_LOCATION);
+        }
 
+//    Define location
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+
+
+    }
+//    Class to show lat and lng
+    private class MyLocationListener implements LocationListener {
+
+        final TextView txt = (TextView) findViewById(R.id.text);
+        @Override
+        public void onLocationChanged(Location loc) {
+            txt.setText("Location changed: Lat: " + loc.getLatitude() + " Lng: "
+                    + loc.getLongitude());
+            Toast.makeText(
+                    getBaseContext(),
+                    "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+                            + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " + loc.getLongitude();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
     }
 }
